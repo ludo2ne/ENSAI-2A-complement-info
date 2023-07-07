f77e24bdb9bdeabdc2ca6d1cc2e13abd43042142
Ludovic Deneuville <ludovic.deneuville@ensai.fr>
2023-07-07 15:16:56 +0200

    q3

--- src/__main__.py
+++
@@ -1,8 +0,0 @@
-
-from business_object.pokemon.pokemon import Pokemon
-from business_object.statistic import Statistic
-
-stats_pk1 = Statistic(100, 10, 10, 10, 10, 10)
-pk1 = Pokemon(name='pika', stat_current=stats_pk1, type_pk='Attacker')
-
-print(pk1)
---
+++ src/test/test_business_object/test_pokemon/test_defender_pokemon.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from business_object.pokemon.defender_pokemon import DefenderPokemon
+from business_object.statistic import Statistic
+
+
+class TestDefenderPokemon(TestCase):
+    def test_get_coef_damage_type(self):
+        # GIVEN
+        attack = 100
+        defense = 100
+        snorlax = DefenderPokemon(stat_current=Statistic(
+            attack=attack,
+            defense=defense
+        ))
+        # WHEN
+        multiplier = snorlax.get_pokemon_attack_coef()
+        # THEN
+        self.assertEqual(2, multiplier)
