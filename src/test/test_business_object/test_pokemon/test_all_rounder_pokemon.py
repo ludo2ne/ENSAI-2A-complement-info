6df34d51a9a5991deffcc1ce106600e582188873
Ludovic Deneuville <ludovic.deneuville@ensai.fr>
2023-07-07 15:21:23 +0200

    q3 correction

---
+++ src/test/test_business_object/test_pokemon/test_all_rounder_pokemon.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from business_object.pokemon.all_rounder_pokemon import AllRounderPokemon
+from business_object.statistic import Statistic
+
+
+class TestAllRounderPokemon(TestCase):
+    def test_get_coef_damage_type(self):
+        # GIVEN
+        spe_atk = 100
+        spe_def = 100
+        charizard = AllRounderPokemon(stat_current=Statistic(
+            sp_atk=spe_atk,
+            sp_def=spe_def
+        ))
+        # WHEN
+        multiplier = charizard.get_pokemon_attack_coef()
+        # THEN
+        self.assertEqual(2, multiplier)
